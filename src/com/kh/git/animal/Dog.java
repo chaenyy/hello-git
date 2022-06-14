2d7322042262d8a6b0082a18985089f456a1d4da
chaenyy <chaeeun0119@naver.com>
2022-06-14 12:49:07 +0900

    merge conflict

--- src/com/kh/git/animal/Dog.java
+++ src/com/kh/git/animal/Dog.java
@@ -1,11 +1,8 @@
 package com.kh.git.animal;
 
 public class Dog {
-<<<<<<< HEAD
-=======
 	private String name;
 	
->>>>>>> refs/heads/animal
 	public void bark() {
 		System.out.println("멍멍!");
 		System.out.println("왈왈!");
